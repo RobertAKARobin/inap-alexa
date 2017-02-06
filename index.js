565179cf7db11b05704a7c798bb2a4728a7257a8
RobertAKARobin <robertgfthomas@gmail.com>
2017-02-06 06:09:10 -0600

    Minor naming changes

--- index.js
+++ index.js
@@ -2,28 +2,23 @@
 
 var https = require('https');
 var alexa = require('alexa-app')
-var app = new alexa.app('INeedAPrompt');
+var app = new alexa.app('I Need A Prompt');
 
-app.intent('Prompt', {
-		'utterances': [
-			'Prompt me',
-			'Prompt please'
-		]
-	},
-	function(request, response, callback){
-		var aggregate = '';
-		https.get('https://ineedaprompt.com/api/text', function(api){
-		  	api.on('data', function(data){
-		       	aggregate += data;
-		  	});
-		  	api.on('end', function(){
-		       	console.log(aggregate);
-		       	response.say(aggregate);
-		       	response.send();
-		  	});
-		});
-		return false;
-	}
-);
+app.intent('PromptMe', {}, sendPrompt);
 
 exports.handler = app.lambda();
+
+function sendPrompt(request, response, callback){
+	var aggregate = '';
+	https.get('https://ineedaprompt.com/api/text', function(api){
+		api.on('data', function(data){
+			aggregate += data;
+		});
+		api.on('end', function(){
+			console.log(aggregate);
+			response.say(aggregate);
+			response.send();
+		});
+	});
+	return false;
+}
